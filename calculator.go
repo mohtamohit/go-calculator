0cd6ee33d51738f0f97a7d78306535227a5369cf
Mohit Mohta <mohtamohit@gmail.com>
2018-05-17 15:28:46 +0530

    Added Test for Add and an add function
    The test for add at this stage fails when we run go test.  The command go test automates the execution of any test function which is found in "*_test.go" corresponding to the package under the test.

--- calculator.go
+++ calculator.go
@@ -1,1 +1,6 @@
 package calculator
+
+// Add performs addition of two numbers
+func Add(x, y int) int {
+	return 0
+}
--- calculator_test.go
+++ calculator_test.go
@@ -3,7 +3,11 @@ package calculator
 import "testing"
 
 func TestCanAdd(t *testing.T) {
-
+	result := Add(2, 3)
+	if result != 5 {
+		t.Log("Addition of 2 and 3 should yield 5 but yielded", result)
+		t.Fail()
+	}
 }
 
 func TestCanSubtract(t *testing.T) {
