ec8b5d2308184b7c597f9246230fc441158a791c
Mohit Mohta <mohtamohit@gmail.com>
2018-05-17 19:52:42 +0530

    Replaced logAndAssert with require
    I also want to learn the use of assert package and the way it is used, I mean some example circumstances where we should prefer using assert instead of acquire.
    
    I know what is the difference between the two though. Assert return bool and doesn't execute t.Fail() in case of mismatch while require executes t.Fail() in case of mismatch.

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -1,40 +1,35 @@
 package calculator
 
-import "testing"
+import (
+	"testing"
 
-func logAndAssert(expected, actual int, message string, t *testing.T) {
-	if expected != actual {
-		t.Log(message, actual)
-		t.Fail()
-	}
-}
-func TestCanAdd(t *testing.T) {
+	"github.com/stretchr/testify/require"
+)
+
+func TestAdd(t *testing.T) {
 	result := Add(2, 3)
-	logAndAssert(5, result, "Addition of 2 and 3 should yield 5 but yielded", t)
+	require.Equal(t, 5, result, "Addition of 2 and 3 should yield 5 but yieled %d", result)
 }
 
-func TestCanSubtract(t *testing.T) {
+func TestSubtract(t *testing.T) {
 	result := Subtract(5, 1)
-	logAndAssert(4, result, "Subtraction of 1 from 5 should yield 4 but yielded", t)
+	require.Equal(t, 4, result, "Subtraction of 1 from 5 should yield 4 but yielded %d", result)
 }
 
-func TestCanMultiply(t *testing.T) {
+func TestMultiply(t *testing.T) {
 	result := Multiply(2, 3)
-	logAndAssert(6, result, "Multiplication of 2 and 3 should yield 3 but yielded", t)
+	require.Equal(t, 6, result, "Multiplication of 2 and 3 should yield 6 but yielded %d", result)
 }
 
-func TestCanDivide(t *testing.T) {
+func TestDivide(t *testing.T) {
 	result, err := Divide(6, 3)
 	if err != nil {
 		t.Log("An error occured: ", err.Error())
 	}
-	logAndAssert(2, result, "Division of 6 by 3 should yield 2 but yielded,", t)
+	require.Equal(t, 2, result, "Division of 6 and 3 should yield 2 but yielded %d", result)
 }
 
 func TestDivisionByZeroNotAllowed(t *testing.T) {
 	_, err := Divide(6, 0)
-	if err.Error() != "Division by zero is not allowed" {
-		t.Log("Wrong error message for division by zero")
-		t.Fail()
-	}
+	require.EqualError(t, err, "Division by zero is not allowed", "Wrong error message")
 }
