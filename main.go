3e5143398fad5f2cc5f69a2c71e7af6ea2f4a250
Mohit Mohta <mohtamohit@gmail.com>
2018-05-17 17:44:47 +0530

    Add CLI Client for the calculator

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"practice/go-calculator/calculator"
+	"strconv"
+)
+
+func calculate(x int, y int, operation string) int {
+	switch operation {
+	case "ADD", "add":
+		return calculator.Add(x, y)
+	case "SUB", "sub":
+		return calculator.Subtract(x, y)
+	case "MUL", "mul":
+		return calculator.Multiply(x, y)
+	case "DIV", "div":
+		res, _ := calculator.Divide(x, y)
+		return res
+	}
+	fmt.Println("Please enter a valid operation.")
+	return -1
+}
+
+func main() {
+	var s1, s2, s3 string
+
+	for {
+		fmt.Println("Enter your command (eg. 1 2 ADD, 2 3 SUB, 1 3 MUL, 6 2 DIV): ")
+		fmt.Scanln(&s1, &s2, &s3)
+
+		num1, err := strconv.Atoi(s1)
+		if err != nil {
+			fmt.Println("error detected")
+		}
+
+		num2, err := strconv.Atoi(s2)
+		if err != nil {
+			fmt.Println("error detected")
+		}
+
+		fmt.Println(calculate(num1, num2, s3))
+	}
+}
