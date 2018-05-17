2c25f15907fb4489bf5e84593626465041199743
Mohit Mohta <mohtamohit@gmail.com>
2018-05-17 15:11:03 +0530

    Added the basic suite for Calculator

---
+++ calculator_test.go
@@ -0,0 +1,19 @@
+package calculator
+
+import "testing"
+
+func TestCanAdd(t *testing.T) {
+
+}
+
+func TestCanSubtract(t *testing.T) {
+
+}
+
+func TestCanMultiply(t *testing.T) {
+
+}
+
+func TestCanDivide(t *testing.T) {
+
+}
