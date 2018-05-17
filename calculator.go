0e35b0d672a04999a85dadf6d7b8146a18acb1fc
Mohit Mohta <mohtamohit@gmail.com>
2018-05-17 16:00:16 +0530

    Added other functions (sub, mult, div) and their tests.

--- calculator.go
+++ calculator.go
@@ -4,3 +4,18 @@ package calculator
 func Add(x, y int) int {
 	return x + y
 }
+
+// Subtract performs subtraction of two numbers
+func Subtract(x, y int) int {
+	return x - y
+}
+
+// Multiply performs multiplication of two numbers
+func Multiply(x, y int) int {
+	return x * y
+}
+
+// Divide performs division of two numbers
+func Divide(x, y int) int {
+	return x / y
+}
--- calculator_test.go
+++ calculator_test.go
@@ -14,13 +14,16 @@ func TestCanAdd(t *testing.T) {
 }
 
 func TestCanSubtract(t *testing.T) {
-
+	result := Subtract(5, 1)
+	logAndAssert(4, result, "Subtraction of 1 from 5 should yield 4 but yielded", t)
 }
 
 func TestCanMultiply(t *testing.T) {
-
+	result := Multiply(2, 3)
+	logAndAssert(6, result, "Multiplication of 2 and 3 should yield 3 but yielded", t)
 }
 
 func TestCanDivide(t *testing.T) {
-
+	result := Divide(6, 3)
+	logAndAssert(2, result, "Division of 6 by 3 should yield 2 but yielded,", t)
 }
